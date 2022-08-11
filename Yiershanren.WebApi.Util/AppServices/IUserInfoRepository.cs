041bde94892833589cc4d23a5c25fef6ef0ad4f9
Yiershanren <85404200+Yiershanren@users.noreply.github.com>
2022-08-11 19:32:11 +0800

    Add files via upload

---
+++ Yiershanren.WebApi.Util/AppServices/IUserInfoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yiershanren.WebApi.Util
+{
+    public interface IUserInfoRepository
+    {
+        /// <summary>
+        /// 根据用户id获取用户信息
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <returns></returns>
+        UserInfo GetUserInfoById(int id);
+    }
+}
---
+++ Yiershanren.WebApi.Util/AppServices/UserInfo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Dapper.Contrib.Extensions;
+
+namespace Yiershanren.WebApi.Util
+{
+    [Table("UserInfo")]
+    public class UserInfo
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 账号名称
+        /// </summary>
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 账号密码
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 手机号码
+        /// </summary>
+        public string MobilePhone { get; set; }
+
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public string DoctorName { get; set; }
+
+        /// <summary>
+        /// 用户简称
+        /// </summary>
+        public string DoctorShortName { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string CreateBy { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? CreateDate { get; set; }
+
+        /// <summary>
+        /// 修改人
+        /// </summary>
+        public string UpdateBy { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? UpdateDate { get; set; }
+
+        /// <summary>
+        /// 用户等级
+        /// </summary>
+        public int? DoctorLevel { get; set; }
+
+        /// <summary>
+        /// 用户星标
+        /// </summary>
+        public int? DoctorStar { get; set; }
+    }
+}
